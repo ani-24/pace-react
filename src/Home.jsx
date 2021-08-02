242a533a0057fbacf7efbd757e8a481d47bc711e
Aniket <rajeshwarkumar9204@gmail.com>
2021-08-02 06:49:33 +0530

    create carousel

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header  from './Header';
+import React from "react";
+import Header from "./Header";
+import Slider from "./Slider";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Slider />
     </>
-  )
-}
+  );
+};
 
 export default Home;
---
+++ src/Slider.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ImgData from "./Main-slider-data";
+
+const slidercells = (img) => {
+  return (
+    <>
+      <div className="carousel-img">
+        <img src={img.src} alt={img.alt} />
+      </div>
+    </>
+  );
+};
+
+const Slider = () => {
+  return (
+    <>
+      <div className="carousel" data-flickity>
+        {ImgData.map(slidercells)}
+      </div>
+    </>
+  );
+};
+
+export default Slider;
