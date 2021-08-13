05447566b7d958ecfcf49671d7ed4f19767a5735
Aniket <rajeshwarkumar9204@gmail.com>
2021-08-13 14:54:43 +0530

    add the courses and create a new format for courses

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,13 +3,37 @@ import Header from "../components/Header";
 import CoursesData from "../data/Course-data";
 import Footer from "../components/Footer";
 
-const createCards = (course) => {
+const createCourses = (course, idx) => {
+  const listStyle = {
+    paddingLeft: "1em",
+  };
   return (
     <>
-      <div className="course">
+      <div className="course" key={idx}>
         <div className="course-body">
           <div className="course-title">{course.title}</div>
-          <div className="course-desc">{course.desc}</div>
+          <div className="course-desc">
+            Online or offline whatever students prefer
+            <br />
+            Only 20 students in a batch
+            <br />
+            <br />
+            <ol style={listStyle}>
+              {course.items.map((item) => {
+                return (
+                  <>
+                    <li>{item}</li>
+                  </>
+                );
+              })}
+            </ol>
+            <br />
+            The full training is under the guidance of <b>
+              Mr. Pratap Manish
+            </b>{" "}
+            (IIM alumni with more than 15 years of experience in multiple
+            countries). <br /> Fee to be paid at the time of admission.
+          </div>
         </div>
         <div className="course-footer">
           <div className="footer-details course-duration">
@@ -25,7 +49,7 @@ const createCards = (course) => {
 const CourseGallery = () => {
   return (
     <>
-      <div className="course-gallery">{CoursesData.map(createCards)}</div>
+      <div className="course-gallery">{CoursesData.map(createCourses)}</div>
     </>
   );
 };
@@ -34,6 +58,11 @@ const Courses = () => {
   return (
     <>
       <Header />
+      <p className="intro-text">
+        PACE Institute offers multiple offline or online classes for short or
+        long duration for English language and personality development for all
+        age groups and different levels as per the requirement of people
+      </p>
       <CourseGallery />
       <Footer />
     </>
