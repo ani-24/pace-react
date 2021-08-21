e3a13a3821077b3736719ce70fe6c6c93b796e7d
Aniket <rajeshwarkumar9204@gmail.com>
2021-08-21 09:13:23 +0530

    create image-gallery page and add stylings to it

---
+++ src/data/Image-gallery-data.jsx
@@ -0,0 +1,44 @@
+const ImageGalleryImgs = [
+  {
+    img: "./img/image-gallery/img1.jpg",
+    alt: "Pratap Manish",
+  },
+  {
+    img: "./img/image-gallery/img2.jpg",
+    alt: "Office",
+  },
+  {
+    img: "./img/image-gallery/img4.jpg",
+    alt: "Office",
+  },
+  {
+    img: "./img/image-gallery/img6.jpg",
+    alt: "Corridor",
+  },
+  {
+    img: "./img/image-gallery/img7.jpg",
+    alt: "Classroom",
+  },
+  {
+    img: "./img/image-gallery/img8.jpg",
+    alt: "Classroom",
+  },
+  {
+    img: "./img/image-gallery/img10.jpg",
+    alt: "Independence Day Celebratior",
+  },
+  {
+    img: "./img/image-gallery/img11.jpg",
+    alt: "Pace's Students",
+  },
+  {
+    img: "./img/image-gallery/img5.jpg",
+    alt: "Stairs",
+  },
+  {
+    img: "./img/image-gallery/img9.jpg",
+    alt: "Pace Tower",
+  },
+];
+
+export default ImageGalleryImgs;
--- src/pages/Image-gallery.jsx
+++ src/pages/Image-gallery.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Header from "../components/Header";
+import ImageGalleryImgs from "../data/Image-gallery-data";
+
+const Gallery = () => {
+  return (
+    <>
+      <div className="image-gallery">
+        {ImageGalleryImgs.map((img) => {
+          return (
+            <>
+              <div className="gallery-item">
+                <img src={img.img} alt={img.alt} />
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 const ImageGallery = () => {
   return (
     <>
       <Header />
+      <Gallery />
     </>
   );
 };
