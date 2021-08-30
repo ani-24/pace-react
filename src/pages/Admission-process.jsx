f7e9dba700bb67cd71ea70e1e6123003e8c5d395
Aniket <rajeshwarkumar9204@gmail.com>
2021-08-30 15:19:24 +0530

    create admission process pgae

--- src/pages/Admission-process.jsx
+++ src/pages/Admission-process.jsx
@@ -3,11 +3,46 @@ import Header from "./../components/Header";
 import Footer from "./../components/Footer";
 import Wame from "./../components/Wame";
 import JumpToTop from "./../components/Jump-to-top";
+
+const Content = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="text">
+          <h1 className="title">Admission details:</h1>
+          <p>
+            Limited Seats are available for the admission to courses at PACE
+            Institute.
+          </p>
+          <p>
+            Full fee of the course is to be deposited at the time of admission.
+          </p>
+          <p>
+            2 passport size photos and copy of Aadhar card is to be submitted.
+          </p>
+          <p>
+            Payment options:
+            <ul className="payment-options">
+              <li className="option">&rarr; UPI</li>
+              <li className="option">&rarr; IMPS</li>
+              <li className="option">&rarr; NEFT</li>
+              <li className="option">&rarr; Cash (offline)</li>
+            </ul>
+          </p>
+        </div>
+        <div className="img">
+          <img src="./img/payment.png" alt="Payment" />
+        </div>
+      </div>
+    </>
+  );
+};
+
 const AdmissionProcess = () => {
   return (
     <>
       <Header />
-
+      <Content />
       <Footer />
       <Wame />
       <JumpToTop />
